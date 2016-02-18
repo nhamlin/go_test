176dcb74f65ff6f7f4344f1266c88245205c3a45
Nick Hamlin <nicholas_hamlin@hotmail.com>
2016-02-18 13:55:14 -0600

    Chapter 8: TCP Listener

--- src/golang_book/chapter8/tcp.go
+++ src/golang_book/chapter8/tcp.go
@@ -6,42 +6,44 @@ import (
 	"net"
 )
 
+// Set up a TCP listening server
 func server() {
-	ln, err := net.Listen("tcp", ":9999")
+	ln, err := net.Listen("tcp", ":9999") // Listen on port 9999 for TCP traffic
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	for {
-		c, err := ln.Accept()
+		c, err := ln.Accept() // Accept a connection
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
-		go handleServerConnection(c)
+		go handleServerConnection(c) // Handle the connection
 	}
 }
 
+// What do we do with the connection?
 func handleServerConnection(c net.Conn) {
-	var msg string
-	err := gob.NewDecoder(c).Decode(&msg)
+	var msg string                        // Receive the message
+	err := gob.NewDecoder(c).Decode(&msg) // Decode the message
 	if err != nil {
 		fmt.Println(err)
 	} else {
-		fmt.Println("Received", msg)
+		fmt.Println("Received", msg) // Print out the message that was received
 	}
 	c.Close()
 }
 
 func client(msg string) {
-	c, err := net.Dial("tcp", "127.0.0.1:9999")
+	c, err := net.Dial("tcp", "127.0.0.1:9999") // Connect to a server, localhost port 9999
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	// msg := "Hello, World"
-	fmt.Println("Sending", msg)
-	err = gob.NewEncoder(c).Encode(msg)
+	fmt.Println("Sending", msg)         // Print the message that is being sent
+	err = gob.NewEncoder(c).Encode(msg) // Encode the message
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -50,9 +52,9 @@ func client(msg string) {
 }
 
 func main() {
-	go server()
+	go server() // Create the server
 	var input string
 	fmt.Scanln(&input)
-	go client(input)
+	go client(input) // Create the client with the message that was entered on the scan line
 	fmt.Scanln(&input)
 }
